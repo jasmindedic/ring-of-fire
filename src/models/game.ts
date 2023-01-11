88eb7f38475463c31bd5d6ef021d53550a1a1314
Jasmin Dedic <jdedic1999@gmail.com>
2023-01-11 10:15:06 +0100

    Changed card image

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,29 +1,29 @@
 export class Game {
-    public players: string[] = ['player 1', 'player 2','player 3'];
-    public stack: string[] = [];
-    public playedCards: string[] = [];
-    public currentPlayer: number = 0;
+  public players: string[] = ['player 1', 'player 2', 'player 3'];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
 
-    constructor(){
+  constructor() {
 
-        for(let i=1; i < 14; i++){
-            this.stack.push('ace_' + i);
-            this.stack.push('clubs_' + i);
-            this.stack.push('diamonds_' + i);
-            this.stack.push('hearts_' + i)
-        }
-        
-        this.stack = shuffle(this.stack);
-        
-    } 
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('spades_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+      this.stack.push('hearts_' + i)
+    }
+
+    this.stack = shuffle(this.stack);
+
+  }
 }
 
 
 
-  function shuffle(array: any) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+function shuffle(array: any) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
+}
